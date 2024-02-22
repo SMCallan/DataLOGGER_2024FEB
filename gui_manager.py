87de2ea62ce4ec41693890a3f357db4714408649
Cal__ Nutella on Toast __MaxD <126923185+SMCallan@users.noreply.github.com>
2024-02-22 15:09:30 +0000

    Add files via upload

--- gui_manager.py
+++ gui_manager.py
@@ -13,11 +13,18 @@ class GUIManager:
         self.canvas = FigureCanvasTkAgg(self.figure, master=self.root)
         self.sensor_data = {sensor_id: [] for sensor_id in sensor_manager.get_sensor_ids()}
         self.setup_ui()
+        self.auto_update_interval = 1000  # Interval in milliseconds
 
+    def start_auto_update(self):
+        self.update_sensor_readings()  # Update the sensor readings
+        # Schedule the next call to this method after the specified interval
+        self.root.after(self.auto_update_interval, self.start_auto_update)
+    
     def setup_ui(self):
         self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
         self.update_button = tk.Button(self.root, text="Read Sensors", command=self.update_sensor_readings)
         self.update_button.pack(side=tk.BOTTOM)
+        self.start_auto_update()  # Start automatic updates
 
     def update_sensor_readings(self):
         for sensor_id in self.sensor_manager.get_sensor_ids():
